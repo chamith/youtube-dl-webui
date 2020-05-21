3949a4644636e220006f541a672a1dc960c45342
Chamith Siriwardena <chamith@gmail.com>
2020-05-21 09:38:40 +0530

    layout changes

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -3,17 +3,10 @@ import Item from './Item'
 export default class ItemList extends Component {
     render() {
         return (
-            <div className="divTable" style={{borderStyle:'none'}}>
-                {/* <div className="divTableHeading">
-                    <div className="divTableHead" style={{width:32}}>Status</div>
-                    <div className="divTableHead">Title</div>
-                    <div className="divTableHead">Progress</div>
-                </div> */}
-                <div className="divTableBody">
+                 <div className="itemList">
                     {this.props.items.map(item => (
                         <Item key={item.id} item={item} />))}
                 </div>
-            </div>
         )
     }
 }
--- src/components/Request.js
+++ src/components/Request.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import ItemList from './ItemList';
 import Modal from 'react-modal';
+// import Collapsible from 'react-collapsible'
+
 export default class Request extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      isModalOpen: false
+      isModalOpen: false,
+      isItemListOpen: false
     }
 
   }
@@ -14,7 +17,7 @@ export default class Request extends Component {
 
     const status = this.props.request.status;
 
-    if( status > 0 && status < 3 )
+    if (status > 0 && status < 3)
       return 'downloading';
     else if (status === 0)
       return 'pending';
@@ -36,12 +39,12 @@ export default class Request extends Component {
 
     return (
       <>
-        <div className="divTableRow">
+        <div className="divTableRow" onClick={() => { this.setState({ isItemListOpen: !this.state.isItemListOpen }) }}>
           <div className={`divTableCell`}><img width='24px' src={`${process.env.PUBLIC_URL}/${this.statusText()}.png`} alt="status icon" />{this.progressText()} {request.url}</div>
-          <div className={`divTableCell`}><button value={request.id} onClick={(event) => this.setState({ isModalOpen: true })}><img alt='delete icon' width='24px' src={process.env.PUBLIC_URL + '/delete.png'} /></button>
-            <Modal isOpen={this.state.isModalOpen} style={{ overlay: { backgroundColor: 'grey' }, content: { border: 1, width: '40%', height: '20%', left: '30%', top: '40%' } }}>
+          <div className={`divTableCell`}><button onClick={(event) => this.setState({ isModalOpen: true })}><img alt='delete icon' width='24px' src={process.env.PUBLIC_URL + '/delete.png'} /></button>
+            <Modal isOpen={this.state.isModalOpen} style={{ overlay: { backgroundColor: 'grey' }, content: { border: 1, width: '40%', height: '20%', left: '30%', top: '40%' } }} >
               <h2>Confirm Deletion</h2>
-              <div>Are you sure to delete the request '{request.url}'</div>
+              <div>Are you sure to delete the request <b>'{request.url}'</b></div>
               <div>&nbsp;</div>
               <div>
                 <button onClick={() => this.props.onDelete(request.id)}>Submit</button>&nbsp;
@@ -50,10 +53,9 @@ export default class Request extends Component {
             </Modal>
           </div>
         </div>
-        <div className="divTableRow">
-          <span className="divTableCell itemList"><ItemList items={request.items} /></span>
-          <span className="divTableCell itemList"></span>
-        </div>
+        {/* <Collapsible className="itemList" open={this.state.isItemListOpen} > */}
+        <ItemList items={request.items} />
+        {/* </Collapsible> */}
       </>
     )
   }
